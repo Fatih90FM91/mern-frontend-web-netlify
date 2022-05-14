ffbc90f26872fd4ff7e20423bfa604cdca1552a7
Fatih90FM91 <hasan9091.fs@gmail.com>
2022-05-14 02:07:28 +0200

    adding

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,8 @@ const devENV = process.env.NODE_ENV !== "production";
 const {REACT_APP_DEV_API  , REACT_APP_PROD_API} = process.env;
 
 console.log(REACT_APP_PROD_API)
+console.log(devENV);
+console.log(process.env.NODE_ENV);
 
 
 export default class Login extends Component {
